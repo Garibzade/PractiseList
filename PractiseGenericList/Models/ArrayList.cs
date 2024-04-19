cc74eebdc909d185a2a5f38b741a0e14dd382313
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-19 07:26:42 +0400

    Added Practise

---
+++ PractiseGenericList/Models/ArrayList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseGenericList.Models
+{
+    internal class GenericList<T>
+    {
+        T[] _arr;
+        public int Capacity { get; set; }
+        public int Count { get; private set; }
+        private Type _listType;
+        public GenericList(Type type)
+        {
+           
+
+            _arr = new T[Capacity];
+            _listType = type;
+        }
+        public GenericList(int capacity, Type type)
+        {
+            Capacity = capacity;
+            _arr = new T[Capacity];
+            _listType = type;
+        }
+
+        public GenericList()
+        {
+        }
+
+        public T this[int num]
+        {
+            get { return _arr[Count]; }
+            set { _arr[Count] = value; }
+
+        }
+        public void Add(T item)
+        {
+            //if (item.GetType().Name == _listType.Name) {
+                if (_arr.Length == Count)
+                {
+                    Array.Resize(ref _arr, Capacity + Count);
+                }
+        //        _arr[Count] = item;
+        //        Count++; 
+
+        //}
+            else { throw new Exception(); }
+        }
+        public void GetInfo()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(_arr[i]);
+            }
+        }
+    }
+}
---
+++ PractiseGenericList/Program.cs
@@ -0,0 +1,22 @@
+﻿using PractiseGenericList.Models;
+using System.Collections;
+
+namespace PractiseGenericList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList list = new ArrayList(typeof(Int32));
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Add(4);
+            //list.Add("salam");
+
+            GenericList<int> IntArr = new GenericList<int>();
+            IntArr.Add(12);
+
+        }
+    }
+}
